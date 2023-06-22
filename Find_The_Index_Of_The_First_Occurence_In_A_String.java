072699c6a76b1d0ade075870b0ed6e1066400ba3
raghavgarg1996 <raghav.gargril291900@gmail.com>
2023-06-22 12:23:29 +0530

    leetcode question 28

--- Find_The_Index_Of_The_First_Occurence_In_A_String.java
+++ Find_The_Index_Of_The_First_Occurence_In_A_String.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Find_The_Index_Of_The_First_Occurence_In_A_String {
+public class Find_The_Index_Of_The_First_Occurence_In_A_String {
+
+    // Partial solution - successfully completeled 29 cases, below case failed
+    public static int strStr(String haystack, String needle) {
+
+        int x = 0;
+        int y = 0;
+        int startPos = -1;
+        int endPos = 0;
+        int count =0;
+
+        if (haystack.length()<needle.length()) {
+            return startPos;
+        }
+
+        while (y<haystack.length()) {
+            if (needle.charAt(x)==haystack.charAt(y)) {
+                x++;
+                y++;
+                count++;
+                if (count==needle.length()) {
+                    endPos =y;
+                    startPos = endPos - needle.length();
+                    break;
+                }
+            } else {
+                x=0;
+                if (needle.charAt(x)==haystack.charAt(y)) {
+                    x++;
+                    y++;
+                    count++;
+                    if (count==needle.length()) {
+                        endPos =y;
+                        startPos = endPos - needle.length();
+                        break;
+                    }
+                } else {
+                    y++;
+                }
+            }
+        }
+
+        return startPos;
+    }
+
+    public static void main(String[] args) {
+
+        String haystack = "mississippi";
+        String needle = "issip";
+
+        System.out.println(strStr(haystack, needle));
+    }
 }
