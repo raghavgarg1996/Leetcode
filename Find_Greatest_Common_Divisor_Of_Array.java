3631f4ee319babab1b5024c35d16ab1544338f3c
raghavgarg1996 <raghav.gargril291900@gmail.com>
2023-07-12 15:32:08 +0530

    Updating code

--- Find_Greatest_Common_Divisor_Of_Array.java
+++ Find_Greatest_Common_Divisor_Of_Array.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Find_Greatest_Common_Divisor_Of_Array {
+public class Find_Greatest_Common_Divisor_Of_Array {
+
+    public static int findGCD(int[] nums) {
+
+        int maxValue =0;
+        int minValue = 99999;
+        int hcf =0;
+
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i]>maxValue) {
+                maxValue =  nums[i];
+            }
+        }
+
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i]<minValue) {
+                minValue = nums[i];
+            }
+        }
+
+
+        for (int i=1; i<=minValue|| i<=maxValue; i++) {
+            if ((minValue % i ==0) && (maxValue % i ==0)) {
+                hcf = i;
+            }
+        }
+
+        return hcf;
+    }
+
+    public static  void main (String[] args) {
+
+        int[] nums = {2,5,6,9,10};
+        System.out.println(findGCD(nums));
+    }
 }
