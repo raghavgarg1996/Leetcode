c85fb2c343414c1b6e861917155154787bd81b83
raghavgarg1996 <raghav.gargril291900@gmail.com>
2023-09-10 17:40:37 +0530

    Enhance code

--- Code_test.java
+++ Code_test.java
@@ -3,5 +3,6 @@ public class Code_test {
     public static  void main (String[] args) {
 
         System.out.println("code worked");
+        System.out.println("Task worked");
     }
 }
--- Valid_anagrams.java
+++ Valid_anagrams.java
@@ -1,3 +1,4 @@
+
 import java.util.Arrays;
 
 public class Valid_anagrams {
