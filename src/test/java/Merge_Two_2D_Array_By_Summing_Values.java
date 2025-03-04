c4286cd3b5a86f08cd39e933a4c53eab5330f45d
raghavgarg1996 <raghav.gargril291900@gmail.com>
2025-03-04 11:48:54 +0530

    https://leetcode.com/problems/reverse-string-ii/description/?envType=problem-list-v2&envId=string

---
+++ src/test/java/Merge_Two_2D_Array_By_Summing_Values.java
@@ -0,0 +1,15 @@
+public class Merge_Two_2D_Array_By_Summing_Values {
+
+//    public static int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+//
+//
+//    }
+    public static void main(String[] args) {
+
+        int[][] nums1 = {{1, 2}, {2, 3}, {4, 5}};
+        int[][] nums2 = {{1, 4}, {3, 2}, {4, 1}};
+
+  //      int[][] nums3 = mergeArrays(nums1, nums2);
+
+    }
+}
--- src/test/java/Reverse_String_II.java
+++ src/test/java/Reverse_String_II.java
@@ -1,14 +1,25 @@
 public class Reverse_String_II {
 
     public static String reverseStr(String s, int k) {
+        StringBuilder rever = new StringBuilder();
+        int i = 0;
 
+        while (i < s.length()) {
+            // Reverse first k characters
+            int end = Math.min(i + k, s.length()); // Fix: Handle last segment properly
+            rever.append(new StringBuilder(s.substring(i, end)).reverse());
 
-        for (int i=0; i<s.length(); i++) {
+            // Append next k characters as is
+            int nextPart = Math.min(i + 2 * k, s.length()); // Fix: Ensure we don't exceed length
+            if (end < nextPart) {
+                rever.append(s.substring(end, nextPart));
+            }
 
+            i += 2 * k; // Move to next 2k block
         }
-        return "";
-    }
 
+        return rever.toString();
+    }
     public static void main(String[] args) {
         String s = "abcdefg";
         int k = 2;
