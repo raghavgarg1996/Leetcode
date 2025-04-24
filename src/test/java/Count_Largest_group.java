9ba3593e9e06a32e5317b19b2b83290d77ddaae5
raghavgarg1996 <raghav.gargril291900@gmail.com>
2025-04-24 10:53:50 +0530

    adding pending code

---
+++ src/test/java/Count_Largest_group.java
@@ -0,0 +1,13 @@
+public class Count_Largest_group {
+
+    public static int countLargestGroup(int n) {
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 13;
+        System.out.println(countLargestGroup(n));
+    }
+}
---
+++ src/test/java/Minimum_Number_Of_Operations_To_Make_Element_In_Array_Distinct.java
@@ -0,0 +1,13 @@
+public class Minimum_Number_Of_Operations_To_Make_Element_In_Array_Distinct {
+
+    public static void minimumOperations(int[] nums) {
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {4, 5, 6, 4, 4};
+        minimumOperations(nums);
+    }
+}
