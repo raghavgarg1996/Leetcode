3bcd30a82ac0e807a3845e39d17aae27369fe85c
raghavgarg1996 <raghav.gargril291900@gmail.com>
2026-05-13 12:31:31 +0530

    https://leetcode.com/problems/find-common-characters

--- src/test/java/Find_Common_Characters.java
+++ src/test/java/Find_Common_Characters.java
@@ -1,46 +1,61 @@
-import java.util.HashMap;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class Find_Common_Characters {
 
     public static List<String> commonChars(String[] words) {
 
-        HashMap<Character, Integer> hm = new HashMap<>();
-        List<String> ls = null;
+        String s1 = words[0];
 
-        for (int i = 0; i < words.length; i++) {
-            String s = words[0];
+        char[] c1 = s1.toCharArray();
+        List<String> ls = new ArrayList<>();
 
-            for (int j = 0; j < s.length(); j++) {
-                if (hm.containsKey(s.charAt(j))) {
-                    int val = hm.get(s.charAt(j));
-                    hm.put(s.charAt(j), ++val);
+        if (words.length == 1) {
+            for (int i=0; i<c1.length; i++) {
+                ls.add(String.valueOf(c1[i]));
+            }
+            return ls;
+        }
+
+        String s2 = words[1];
+
+
+
+        for (int i=0; i<c1.length; i++) {
+            if (s2.contains(String.valueOf(c1[i]))) {
+                s2 = s2.replaceFirst(String.valueOf(c1[i]),"");
+                ls.add(String.valueOf(c1[i]));
+            }
+        }
+
+        System.out.println(ls);
+
+        if (words.length == 2) {
+            return ls;
+        }
+
+
+
+        for (int i = 2; i < words.length; i++) {
+            String realWord = words[i];
+            for (int j = 0; j < ls.size(); j++) {
+                if (!realWord.contains(ls.get(j))) {
+                    ls.remove(ls.get(j));
+                    j = j-1;
                 } else {
-                    hm.put(s.charAt(j), 1);
+                    realWord = realWord.replaceFirst(ls.get(j),"");
                 }
             }
-
-            String str = words[i];
-            if (str != s) {
-                for (Map.Entry entry : hm.entrySet()) {
-                    for (int j=0; j<str.length(); j++) {
-                        if (entry.getKey().equals(str.charAt(j))) {
-
-                        }
-                    }
-                }
-            }
-
-
         }
         return ls;
     }
 
-    public static void main(String[] args) {
+        public static void main (String[]args){
 
-        String[] words = {"bella", "label", "roller"};
-        List<String> ls = commonChars(words);
-        System.out.println(ls);
+            String[] words = {"cool"};
+            List<String> ls = commonChars(words);
+            System.out.println(ls);
+        }
     }
-}
